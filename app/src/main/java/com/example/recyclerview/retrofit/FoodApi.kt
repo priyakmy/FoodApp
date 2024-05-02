54f68b47fc9e93ef969d4bec35895dc35ea4163c
priya Kumari <priyakum1071@gmail.com>
2024-05-02 16:37:27 +0530

    commit

--- app/src/main/java/com/example/recyclerview/retrofit/FoodApi.kt
+++ app/src/main/java/com/example/recyclerview/retrofit/FoodApi.kt
@@ -3,11 +3,15 @@ package com.example.recyclerview.retrofit
 import com.example.recyclerview.pojo.MealList
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 
 interface FoodApi {
 
     @GET ("random.php")
     fun getRandomMeal():Call<MealList>
+
+    @GET("lookup.php?")
+    fun getMealDetails(@Query("i")id:String) : Call<MealList>
 
 }
